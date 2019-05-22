da3b235dc7bdb8cf83adf066265fe4a4cfddb9ec
Bryan Richter <b@chreekat.net>
2019-05-22 18:05:41 +0300

    Cleanup; fix prettyprint; avoid duplicating 1st ep

--- shell.nix
+++ shell.nix
@@ -1,17 +1,28 @@
 let
   old = import <nixpkgs> {};
+  ppFixed = old.fetchFromGitHub {
+    owner = "chreekat";
+    repo = "show-prettyprint";
+    rev = "list-fixes";
+    sha256 = "0fxp286wqx6cvqd4x88421p6jpdxs0szdfv0mvzqpz7ikim33d3q";
+  };
   h = old.haskellPackages.extend (self: super: {
     scalpel-core = self.callHackageDirect {
       pkg = "scalpel-core";
       ver = "0.6.0";
       sha256 = "12fy859pwxr42pn9fr3cdbkyjzw2i92vchp2rs6m50f9m8qspa1r";
     } {};
+    #show-prettyprint = self.callPackage ../../src/show-prettyprint {};
+    show-prettyprint = self.callCabal2nix "show-prettyprint" ppFixed {};
   });
 in
 old.mkShell {
-  buildInputs = [(h.ghcWithPackages (p: [
-    p.ghcid
-    p.scalpel-core
-    p.show-prettyprint
-  ]))];
+  buildInputs = [
+    (h.ghcWithPackages (p: [
+      p.ghcid
+      p.scalpel-core
+      p.doctest
+      p.show-prettyprint
+    ]))
+  ];
 }
--- vultr.hs
+++ vultr.hs
@@ -1,48 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 
-{-
-import AllThatServantShit
-type VultrAPI
-    = "v1" :> VultrAPI1
-
-type VultrAPI1
-    = "account" :> "info" :> Get '[JSON] AccountInfo
-    = "app" :> "list" :> Get '[JSON] AppList
-    = "auth" :> "info" :> Get '[JSON] AuthInfo
-
--}
-
 import Control.Applicative
 import Control.Monad
 import Data.Maybe
 import Data.Text (Text)
 import Text.HTML.Scalpel.Core
 import Text.Show.Prettyprint
-import qualified Data.Set as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
-{-
-data Endpoint = Endpoint
-    { path :: Text
-    , description :: Text
-    , needsAPIKey :: Bool
-    , method :: Method
-    , requiredAccess :: RequiredAccess
-    , example :: EndpointExample
-    } deriving (Eq, Show)
-
-data RequiredAccess = Subscriptions | Provisioning | Upgrade | Dns
-    deriving (Eq, Show)
-
-data EndpointExample = EndpointExample
-    deriving (Eq, Show)
-
-data Method = Get | Put
-    deriving (Eq, Show)
--}
-
+-- Selector helpers
 
 t @. cs = AnyTag @: map hasClass cs
 
@@ -50,6 +18,7 @@ div_ = ("div" @.)
 
 id_ k = AnyTag @: ["id" @= T.unpack k]
 
+-- Endpoint
 
 data Endpoint = Endpoint
     { path :: Text
@@ -80,8 +49,15 @@ scrapeEndpoint = do
     scrape2ndTd =
         inSerial (replicateM 2 (seekNext (pure ())) >> seekNext (text "td"))
 
+-- ApiGroup
+
 data ApiGroup = ApiGroup { name :: Text, endpoints :: [Endpoint] }
     deriving (Eq, Ord, Show)
+
+scrapeApiGroup :: Scraper Text ApiGroup
+scrapeApiGroup =
+    ApiGroup <$> (text "h2") <*> chroots ("div" `atDepth` 1) scrapeEndpoint
+    `guardedBy` not . null . endpoints
 
 guardedBy :: (Monad f, Alternative f) => f a -> (a -> Bool) -> f a
 x `guardedBy` b = do
@@ -89,10 +65,8 @@ x `guardedBy` b = do
     x' <$ guard (b x')
 infixl 1 `guardedBy`
 
-scrapeApiGroup :: Scraper Text ApiGroup
-scrapeApiGroup =
-    ApiGroup <$> (text "h2") <*> chroots "div" scrapeEndpoint
-    `guardedBy` not . null . endpoints
+
+-- main helpers
 
 apiGroupRoot = div_ ["main-content"] // div_ ["content-row"]
 
@@ -100,8 +74,6 @@ apiGroupRoot = div_ ["main-content"] // div_ ["content-row"]
 scrap :: Scraper Text a -> IO a
 scrap s = fromJust . flip scrapeStringLike s <$> T.readFile "vultr.html"
 
-main = prettyPrint . M.fromList . take 3 =<< scrap (chroots apiGroupRoot scrapeApiGroup)
-    where
-    sel = div_ ["main-content"] // div_ ["content-row"]
---main = mapM_ prettyPrint =<< (fromJust . flip scrapeStringLike scrapeGroups <$> h)
---main = prettyPrint =<< (fromJust . flip scrapeStringLike (html (AnyTag @: ["id" @= "sshkey_destroy"])) <$> h)
+main =
+    prettyPrint . take 5
+        =<< scrap (chroots apiGroupRoot scrapeApiGroup)
