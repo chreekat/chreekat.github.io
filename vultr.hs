63657d26d54e0d2ee59fcf0e9081fa4c180a0de3
Bryan Richter <b@chreekat.net>
2019-05-22 21:56:30 +0300

    Parse the shit out of crap

--- vultr.hs
+++ vultr.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 
 import Control.Applicative
+import Control.Exception
 import Control.Monad
 import Data.List
 import Data.Maybe
@@ -21,28 +22,60 @@ id_ k = AnyTag @: ["id" @= T.unpack k]
 
 -- Hodonk parsers
 
-parseBool "Yes" = pure True
-parseBool "No" = pure False
-parseBool _ = fail "parseBool"
+failParse who what = error (T.unpack who ++ " couldn't parse: " ++ T.unpack what)
 
-parseMethod "GET" = pure Get
-parseMethod "POST" = pure Post
-parseMethod _ = fail "parseMethod"
+parseBool "Yes" = True
+parseBool "No" = False
+parseBool x = failParse "parseBool" x
 
-parseRequiredAccess "" = pure None
-parseRequiredAccess "billing" = pure Billing
-parseRequiredAccess "dns" = pure Dns
-parseRequiredAccess "firewall" = pure Firewall
-parseRequiredAccess "manage_users" = pure ManageUsers
-parseRequiredAccess "provisioning" = pure Provisioning
-parseRequiredAccess "subscriptions" = pure Subscriptions
-parseRequiredAccess "upgrade" = pure Upgrade
-parseRequiredAccess _ = fail "parseRequiredAccess"
+parseMethod "GET" = Get
+parseMethod "POST" = Post
+parseMethod x = failParse "parseMethod" x
+
+parseRequiredAccess "" = None
+parseRequiredAccess "billing" = Billing
+parseRequiredAccess "dns" = Dns
+parseRequiredAccess "firewall" = Firewall
+parseRequiredAccess "manage_users" = ManageUsers
+parseRequiredAccess "provisioning" = Provisioning
+parseRequiredAccess "subscriptions" = Subscriptions
+parseRequiredAccess "upgrade" = Upgrade
+parseRequiredAccess x = failParse "parseRequiredAccess" x
+
+parseParameter x
+    | (n : t : ds) <- T.words x
+    -- Found three "bugs" in the page! :D
+    = let param t' d o = Just (Parameter n o t' (T.unwords ds))
+          p = case t of
+            "(optional," -> param ParamString (t : ds)
+            "Password"   -> param ParamString (t : ds)
+            -- Unify int and integer
+            "int"         -> param ParamInteger ds
+            -- And one non-bug. n:[t] == "No parameters."
+            "parameters." -> const Nothing
+            _            -> param (parseParamType t) ds
+      in p ("optional" `T.isInfixOf` x)
+    | otherwise = failParse "parseParameter" x
+
+parseParamType "array" = ParamArray
+parseParamType "integer" = ParamInteger
+parseParamType "string" = ParamString
+parseParamType x = failParse "parseParamType" x
 
 -- | Smoosh whitespace. Probably a better way to do this in base...
-parseString = pure . T.intercalate " " . concatMap T.words . T.lines
+parseString = T.intercalate " " . concatMap T.words . T.lines
 
 -- Endpoint
+
+data ParamType = ParamArray  | ParamInteger | ParamString
+    deriving (Eq, Show, Ord)
+
+data Parameter = Parameter
+    { name :: Text
+    , optional :: Bool
+    , typ :: ParamType
+    , description :: Text
+    } deriving (Eq, Show)
 
 data Method = Post | Get
     deriving (Eq, Show)
@@ -65,7 +98,7 @@ data Endpoint = Endpoint
     , method :: Method
     , requiredAccess :: RequiredAccess
     , example :: Example
-    , parameters :: Text
+    , parameters :: [Parameter]
     } deriving (Eq, Show)
 
 data Example = Example { request :: Text, response :: Text }
@@ -86,12 +119,12 @@ scrapeEndpoint = do
     [exReq, exResp, params] <- texts "code"
     Endpoint
         <$> text ("h3" // "a")
-        <*> (parseString =<< text "p")
-        <*> parseBool key
-        <*> parseMethod method
-        <*> parseRequiredAccess acc
+        <*> (parseString <$> text "p")
+        <*> pure (parseBool key)
+        <*> pure (parseMethod method)
+        <*> pure (parseRequiredAccess acc)
         <*> pure (Example exReq exResp)
-        <*> pure params
+        <*> pure (catMaybes (map parseParameter (T.lines params)))
     where
     scrape2ndTd =
         inSerial (replicateM 2 (seekNext (pure ())) >> seekNext (text "td"))
