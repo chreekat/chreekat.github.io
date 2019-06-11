4881bd591fe371058fb30bf4676bab1792ae0d9a
Bryan Richter <b@chreekat.net>
2019-06-11 18:03:50 +0300

    Use new show-prettyprint

--- shell.nix
+++ shell.nix
@@ -1,19 +1,16 @@
 let
   old = import <nixpkgs> {};
-  ppFixed = old.fetchFromGitHub {
-    owner = "chreekat";
-    repo = "show-prettyprint";
-    rev = "list-fixes";
-    sha256 = "0fxp286wqx6cvqd4x88421p6jpdxs0szdfv0mvzqpz7ikim33d3q";
-  };
   h = old.haskellPackages.extend (self: super: {
     scalpel-core = self.callHackageDirect {
       pkg = "scalpel-core";
       ver = "0.6.0";
       sha256 = "12fy859pwxr42pn9fr3cdbkyjzw2i92vchp2rs6m50f9m8qspa1r";
     } {};
-    #show-prettyprint = self.callPackage ../../src/show-prettyprint {};
-    show-prettyprint = self.callCabal2nix "show-prettyprint" ppFixed {};
+    show-prettyprint = self.callHackageDirect {
+      pkg = "show-prettyprint";
+      ver = "0.3";
+      sha256 = "114h5f3lm473vv34nawp53ci580qq31fgbfgg9wal68d11ibl715";
+    } {};
   });
 in
 old.mkShell {
