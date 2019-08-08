b0946499ec27c36fccdc8b31acdbaa5d39efa091
Bryan Richter <b@chreekat.net>
2019-08-08 22:16:35 +0300

    Look at Wats again

--- Vultr.hs
+++ Vultr.hs
@@ -489,10 +489,10 @@ main =
         . filter (isWat . snd . snd)
         -- . filter ((/= NoResponse) . snd. snd)
         -- . filter (((&&) <$> (not . isWat) <*> (/= NoResponse)) . snd . snd)
-        . map (path &&& response . example &&& (parseResponse . response . example)) . concatMap endpoints
+        . map (path &&& edescription &&& (parseResponse . response . example)) . concatMap endpoints
         =<< scrap (chroots apiGroupRoot scrapeApiGroup)
     where
-    isWat (KeyedResponse _) = True
+    isWat (Wat _) = True
     isWat _ = False
 
 newtype ApiKey = ApiKey Text
